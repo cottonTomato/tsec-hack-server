e87f4373117ee456538a05cafd0a0b50103b30b0
Pulkit Dwivedi <pulkitdwivedi123@gmail.com>
2025-01-29 18:48:16 +0530

    webhook verifyable

--- src/controllers/webhook/webhook.route.ts
+++ src/controllers/webhook/webhook.route.ts
@@ -1,3 +1,17 @@
 import { Router } from 'express';
 
 export const webhookRouter = Router();
+
+
+// Add the new webhook GET request handler
+webhookRouter.get('/', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const challenge = req.query['hub.challenge'];
+  const verifyToken = req.query['hub.verify_token'];
+
+  if (mode === 'subscribe' && verifyToken === 'meatyhamhock') {
+    res.status(200).send(challenge);
+  } else {
+    res.status(403).send('Forbidden');
+  }
+});
