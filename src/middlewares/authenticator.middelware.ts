e9b3da2aff544cbf15490ea36e3bb572f878223e
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2025-01-29 18:32:37 +0530

    removed deprecated requireAuth

--- src/middlewares/authenticator.middelware.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response, NextFunction } from 'express';
-import { Unauthenticated } from '../errors';
-
-export function requireAuth(req: Request, _res: Response, next: NextFunction) {
-  if (!req.auth.userId) {
-    throw new Unauthenticated('Not Authenticated');
-  }
-  next();
-}
