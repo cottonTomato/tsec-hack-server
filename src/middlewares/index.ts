3d98797e591a6105f19f56175bb9385ec087e3ce
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2025-01-29 18:34:16 +0530

    removed deprecated requireAuth

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,5 +2,4 @@ export { accessLogger } from './request-logger.middleware';
 export { cacherFactory } from './cacher.middleware';
 export { globalErrorHandler } from './error-handler.middleware';
 export { notFoundHandler } from './not-found.middleware';
-export { requireAuth } from './authenticator.middelware';
 export { validatorFactory } from './validator.middleware';
